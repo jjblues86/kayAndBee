24d81aa6c7ddf77eef1a85e86a6f131450a40034
jjblues86 <jeromejoof@yahoo.com>
2019-07-03 00:07:36 -0700

    working on the routes

--- kaynbee/src/App.js
+++ kaynbee/src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
-import Landing from './Landing/Landing';
+import Landing from './Component/Landing/Landing';
 import NavBar from './Component/NavBar';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
 import Footer from './Component/Footer';
+
+// React Router Import
+import { Switch, Route } from 'react-router-dom';
+// Import Dashboard
+import Home from './Component/Dashboard/Home';
+import Services from './Component/Dashboard/Services';
+import About from './Component/Dashboard/AboutUs';
+import Contacts from './Component/Dashboard/ContactUs';
+
+
 
 import './App.css';
 
@@ -12,6 +21,12 @@ render() {
   return(
       <div className="title">
           <NavBar/>
+          <Switch>
+              <Route path="/home" component={Home}/>
+              <Route path="/services" component={Services}/>
+              <Route path="/about" component={About}/>
+              <Route path="/contacts" component={Contacts}/>
+          </Switch>
           <Landing />
           <br/>
           <Footer/>
---
+++ kaynbee/src/Component/Dashboard/Services.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Services extends React.Component {
+    render() {
+        return(
+            <div>
+                <h2>Services Page </h2>
+            </div>
+        )
+    }
+}
