685e2be222ec077166167f10909c23a9058247d8
jjblues86 <jeromejoof@yahoo.com>
2019-07-14 17:09:48 -0700

    update

--- kaynbee/src/Component/Landing/Landing.js
+++ kaynbee/src/Component/Landing/Landing.js
@@ -4,9 +4,9 @@ import styled from "styled-components";
 export default class Landing extends React.Component {
     render() {
         return(
-            <div className="Landing">
-                {/*<p> Welcome to our site! This is your one stop shop for all of your event planning questions. We are here to serve you and make sure that your idea is turned into reality.</p>*/}
-            </div>
+            {/*<div className="Landing">*/}
+                /*<p> Welcome to our site! This is your one stop shop for all of your event planning questions. We are here to serve you and make sure that your idea is turned into reality.</p>*/
+            // </div>
         )
     }
 }
