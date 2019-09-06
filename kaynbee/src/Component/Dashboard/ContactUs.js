18bda6538d8e76729ddc46b80f58b02c8b345330
jjblues86 <jeromejoof@yahoo.com>
2019-09-06 12:36:16 -0700

    added validation to the contact form

--- kaynbee/src/Component/Dashboard/ContactUs.js
+++ kaynbee/src/Component/Dashboard/ContactUs.js
@@ -11,18 +11,19 @@ export default class ContactUs extends React.Component {
                         <h4><strong>Contact Us</strong></h4>
                         <form action="https://formspree.io/kaddijah-sarjo@live.com" method="POST" >
                             <div className="form-group">
-                                <input type="text" name="_valid[name]" className="form-control" placeholder="Name"/>
+                                <input type="text" name="_valid[name]" required className="form-control" placeholder="Name"/>
                             </div>
                             <div className="form-group">
-                                <input type="email" name="_valid[email]"  className="form-control" placeholder="Email"/>
+                                <input type="email" name="_valid[email]" required className="form-control" placeholder="Email"/>
                             </div>
                             <div className="form-group">
-                                <input type="tel" name="_valid[tel]"  className="form-control" placeholder="Phone"/>
+                                <input type="tel" name="_valid[tel]"  required className="form-control" placeholder="Phone"/>
                             </div>
                             <textarea
                                 type="text"
                                 className="form-control"
                                 name="_valid[message]"
+                                required
                                 cols="30" rows="3" placeholder="Message">
                          </textarea>
 
